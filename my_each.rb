7c78da804d2ff6903b7832189358487c83de8fe5
Dennis Chung <chungdennis6@gmail.com>
2019-02-26 01:44:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
-Def my_each(my_array) # put argument(s) here
+def my_each(my_array) # put argument(s) here
   # code here	 i = 0
   while i < my_array.length
   yield my_array[i]
     i += 1
   end
   my_array
-end 	end 
+end
